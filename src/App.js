968a7cc610a5b904398e8173c7980829838e7aa1
root <root@DESKTOP-M9TM748.localdomain>
2021-07-27 20:33:01 +0800

    change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import './App.css';
+import React from "react";
+import nasdaq from "./data/nasdaq.json";
+import { LineChart, Line, XAxis, YAxis } from 'recharts';
+import "./App.css";
+
+const labels = nasdaq.map((_) => _.Date);
+const nasdaqData = nasdaq.map((_) => _["Adj Close"]);
 
 function App() {
-  return (
-    <div className="App">
-      testing
-    </div>
-  );
+    return (
+        <div className="App">
+            <LineChart width={1200} height={800} data={nasdaq}>
+                <Line type="basis" dataKey="Adj Close" stroke="#8884d8" dot={false}/>
+                <XAxis dataKey="Date" />
+                <YAxis />
+            </LineChart>
+        </div>
+    );
 }
 
 export default App;
